2847395b836b7e451e5379f7b1ff8c35dcda3db7
Raghav B. Venkataramaiyer <bvraghav@gmail.com>
2024-03-04 08:39:06 +0530

    Fill in pixel drawing algorithm.

--- pixel.js
+++ pixel.js
@@ -1,10 +1,32 @@
 function setPixels(sel, pArray, colorHex) {
+  let offset
+
   const rgba = [...hexToRgb(colorHex), 255]
+
+  const canvas = document.querySelector(sel)
+  const ctx = canvas.getContext('2d')
+  const {width: W, height: H}
+	= canvas.getBoundingClientRect()
+  const imData = ctx.getImageData(0,0,W,H)
 
   console.info({
     name: `setPixels`,
-    sel, pArray, colorHex, rgba
+    sel, pArray, colorHex,
+    rgba, canvas, ctx, W, H
   })
+
+  const pixels = imData.data
+  for (const [x,y] of pArray) {
+    try {
+      offset = 4*(y*W + x)
+      pixels.set(rgba, offset)
+    } catch (e) {
+      console.error({at: [x,y]})
+      throw e
+    }
+  }
+
+  ctx.putImageData(imData, 0,0)
 }
 
 function hexToRgb(hex) {
