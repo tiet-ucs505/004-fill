b3285052df07df3ceb928fa5bbe9bc3c8ce7b18c
Raghav B. Venkataramaiyer <bvraghav@gmail.com>
2024-03-04 09:41:37 +0530

    Add BFS.

--- fill.js
+++ fill.js
@@ -4,6 +4,7 @@ function boundaryFill({
   start,
   fillColor,
   normalised = true,
+  adjacency = 4,
 }) {
   console.info({
     name: 'boundaryFill',
@@ -12,6 +13,7 @@ function boundaryFill({
     start,
     fillColor,
     normalised,
+    adjacency,
   })
 
   if (normalised) {
@@ -19,6 +21,102 @@ function boundaryFill({
   }
 
   setPixels(sel, [start], '#ff0000')
+
+  const points = getBoundaryFillPoints({
+    sel, boundaryColor, start, adjacency,
+  })
+  setPixels(sel, points, fillColor)
+}
+
+function getBoundaryFillPoints({
+  sel, boundaryColor, start, adjacency,
+}) {
+  let u, V, vis, points
+
+  console.info({
+    name: `getBoundaryFillPoints`,
+    sel, boundaryColor, start, adjacency,
+  })
+
+  // To check if a point is on boundary
+  // Extract current canvas state
+  const canvas = document.querySelector(sel)
+  const ctx = canvas.getContext('2d')
+  const {width: W, height: H}
+	= canvas.getBoundingClientRect()
+  const imData = ctx.getImageData(0,0,W,H)
+  const pixels = imData.data
+  const arrayEquals = (A,B) => {
+    for(let i=0;i<A.length;++i) {
+      if (A[i] != B[i]) return false
+    }
+    return true
+  }
+  const boundaryRgba = [
+    ...hexToRgb(boundaryColor), 255
+  ]
+  const isBoundary = ([x,y]) => {
+    const canvasRgba = pixels.slice(
+      4*(y*W+x), 4*(y*W+x+1)
+    )
+    return arrayEquals(canvasRgba, boundaryRgba)
+  }
+
+  // Define Adjacency for a given point
+  const ADJ4 = [
+    [0,1], [0,-1], [1,0], [-1,0]
+  ]
+  const ADJ8 = [
+    ...ADJ4,
+    [1,1], [1,-1], [-1,1], [-1,-1]
+  ]
+  const adj = (
+    adjacency==4 ? ADJ4
+      : adjacency==8 ? ADJ8
+      : null
+  )
+  const inView = ([x,y]) => (
+    0 <= x && x < W
+      && 0 <= y && y < H
+  )
+  const getV
+	= ([x,y]) => (adj.map(([a,b])=>([x+a,y+b]))
+		      .filter(inView))
+
+  // Maintain visitation set implemented as a bitmask
+  const isVisited = ([x,y]) => (
+    (vis >> BigInt(y*W+x)) & BigInt(1)
+  )
+  const setVis = ([x,y]) => {
+    vis |= (BigInt(1) << BigInt(y*W+x))
+  }
+
+  // Instantiate Q
+  const Q = []
+  
+  // Initialise BFS
+  vis = BigInt(0)
+  points = []
+  Q.push(start)
+  while (0 < Q.length) {
+    u = Q.shift()
+
+    if (isVisited(u)) {
+      continue
+    }
+
+    setVis(u)
+
+    if (isBoundary(u)) {
+      continue
+    }
+
+    points.push(u)
+
+    Q.push(...getV(u))
+  }
+
+  return points
 }
 
 function floodFill({
